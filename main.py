ca5f052dd8d8f4d7f2686a2d3023ddff4752370e
Jeet_dadhania <47849684+jeet308@users.noreply.github.com>
2021-03-13 10:01:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,64 +1,75 @@
-from fastapi import FastAPI, File, UploadFile, status,Response, Query,Path
-from pydantic import BaseModel,Field
+from fastapi import FastAPI, UploadFile, Form, File
 from typing import Optional
-from fastapi.responses import JSONResponse
-import shutil
+from fastapi.responses import JSONResponse, PlainTextResponse
+import schema as sm
+
+from PIL import Image
 import base64
+import time
 import os
 import io
-from PIL import Image
 import datetime
-import time
 
 app = FastAPI()
+{}
 
-ct = datetime.datetime.now() 
+currentdatetime = datetime.datetime.now()
+chunk_size = (10*1024*1024)
 
 
-@app.post("/uploadfile/",status_code=200)
-async def create_upload_file(
-    response: Response,
-    company_name:str = Query (...,regex="([a-zA-Z])\D*([a-zA-Z])$"),
-    img_width:int = Query(1024,ge=1,le=1920),
-    image_quality_check:bool=True,
-    image_formate:Optional[str] = Query (None,title="jpeg, png, tiff, webp"),
-    ref_id:str = Query (...,regex="^[a-zA-Z0-9]*_?[a-zA-Z0-9]*$",max_length=6),
-    file: UploadFile = File(...),
-    ):
+@app.post("/imagetest/")
+def post_data(
+    reference_id: str = Form(None),
+    resize_width: str = Form('1028'),
+    company_name: str = Form(None),
+    image_format: str = Form(None),
+    quality_check: str = Form('True'),
+    image_file: UploadFile = File(...)
+):
 
-    start_time = time.time()
+    process_start_time = time.time()
     path = os.getcwd()
-    temp_file = os.path.join(path,file.filename)
+    temp_file = os.path.join(path, image_file.filename)
 
-    img = Image.open(temp_file)
-    img_format = img.format
+    image = Image.open(temp_file)
+    #image_format = image.format
 
-    img_file_size = os.path.getsize(temp_file)
+    image_file_size = os.path.getsize(temp_file)
 
-    if (img_file_size<(10*1024*1024)):
-        if (img_format == "JPEG") | (img_format == "PNG"):
-            wpercent = (img_width / float(img.size[0]))
-            hsize = int((float(img.size[1]) * float(wpercent))) 
-            img = img.resize((img_width, hsize), Image.ANTIALIAS)
+    try:
+        input_data = sm.ImageSchema().load(
+            {"reference_id": reference_id,
+             "resize_width": resize_width,
+             "company_name": company_name,
+             "quality_check": quality_check,
+             "image_format": image_format
+             })
+
+        if (image_file_size < chunk_size):
+            wpercent = (input_data['resize_width'] / float(image.size[0]))
+            hsize = int((float(image.size[1]) * float(wpercent)))
+            image = image.resize(
+                (input_data['resize_width'], hsize), Image.ANTIALIAS)
             buf = io.BytesIO()
-            img.save(buf, format='JPEG')
-            base_img = base64.b64encode(buf.getvalue()).decode()
-            response.status_code = status.HTTP_201_CREATED
-            return {"Base64_string":base_img,
-            "ref_id":ref_id,
-            "timestamp":ct,
-            "time":(time.time() - start_time),"status": "Image base string created"}
+            image.save(buf, format='JPEG')
+            base64_string = base64.b64encode(buf.getvalue()).decode()
 
         else:
-            return JSONResponse(status_code=404, content={"message": "File formate not support"})
+            return JSONResponse(status_code=404, content={"message": "File size mast be lessthan 10 MB"})
 
-    else:
-        return JSONResponse(status_code=404, content={"message": "File size mast be lessthan 10 MB"})
+        data = {
+            "basestring": base64_string,
+            "reference_id": input_data['reference_id'],
+            "time_stamp": currentdatetime,
+            "processtime": (time.time() - process_start_time),
+        }
+        status = "success"
+        error = None
+        status_code = 200
+    except sm.ValidationError as e:
+        data = None
+        status = "falied"
+        error = str(e)
+        status_code = 400
 
-
-
-
-
-
-
-
+    return JSONResponse({"data": data, "error": error, status: status}, status_code=status_code)
