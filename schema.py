d63d41586a57880e32d7edec1e8eb26d43ecb28e
Jeet_dadhania <47849684+jeet308@users.noreply.github.com>
2021-03-18 11:58:45 +0530

    Update schema.py

--- schema.py
+++ schema.py
@@ -3,39 +3,34 @@ from datetime import datetime
 
 
 class ImageSchema(Schema):
-    reference_id = fields.Str(example="Foo",
+    reference_id = fields.Str(
         required=True,
-        error_messages={"type":"validation_error",
-                        "field":"reference_id",
-                        "message": "reference_id is required."},
+        error_messages={
+            "message": "reference_id is required."},
         validate=[validate.Length(equal=6)])
     resize_width = fields.Int(required=True,
-        validate=validate.Range(min=1, max=1920),
-        error_messages={"type":"validation_error",
-                        "field":"resize_width",
-                        "message": "resize_width is required."})
+                              validate=validate.Range(min=1, max=1920),
+                              error_messages={
+                                  "message": "resize_width is required."})
+    resize_height = fields.Int(required=True,
+                               validate=validate.Range(min=1, max=1920),
+                               error_messages={
+                                   "message": "resize_height is required."})
     company_name = fields.Str(required=True,
-        validate=validate.Length(min=1, max=30),
-        error_messages={"type":"validation_error",
-                        "field":"company_name",
-                        "message": "company_name is required."})
+                              validate=validate.Length(min=1, max=30),
+                              error_messages={
+                                  "message": "company_name is required."})
     image_format = fields.Str()
     quality_check = fields.Bool()
 
     @validates("image_format")
     def validate_quantity(self, value):
         if value not in ['jpg', 'jpeg', 'png']:
-            raise ValidationError({"type":"validation_error",
-                                   "field":"reference_id",
-                                   "message": f"{value} image_format is not supported"})
+            raise ValidationError({
+                "message": f"{value} image_format is not supported"})
 
     @validates("reference_id")
     def reference_id_validate(self, value):
         if not value.isalnum():
-            raise ValidationError({"type":"validation_error",
-                                   "field":"reference_id",
-                                   "message": "reference_id must be alphanumeric"})
-
-   
-
-
+            raise ValidationError({
+                "message": "reference_id must be alphanumeric"})
