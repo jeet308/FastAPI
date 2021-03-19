4642277ac219a2b49927fea787c07b1631339ffc
Jeet_dadhania <47849684+jeet308@users.noreply.github.com>
2021-03-19 10:40:13 +0530

    Update schema.py

--- schema.py
+++ schema.py
@@ -1,6 +1,6 @@
 from marshmallow import Schema, fields, validate, ValidationError, validates
 from datetime import datetime
-
+from pydantic import BaseModel
 
 class ImageSchema(Schema):
     reference_id = fields.Str(
@@ -12,10 +12,7 @@ class ImageSchema(Schema):
                               validate=validate.Range(min=1, max=1920),
                               error_messages={
                                   "message": "resize_width is required."})
-    resize_height = fields.Int(required=True,
-                               validate=validate.Range(min=1, max=1920),
-                               error_messages={
-                                   "message": "resize_height is required."})
+    
     company_name = fields.Str(required=True,
                               validate=validate.Length(min=1, max=30),
                               error_messages={
@@ -34,3 +31,18 @@ class ImageSchema(Schema):
         if not value.isalnum():
             raise ValidationError({
                 "message": "reference_id must be alphanumeric"})
+
+
+class Example(BaseModel):
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "reference_id":  "abcd12",
+                "resize_width":  1250,
+                "resize_height": 500,
+                "company_name":  "frslabs",
+                "image_format":  "png",
+                "quality_check": True,
+            }
+        }
