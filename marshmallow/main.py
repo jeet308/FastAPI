023b0fa626685f3251696ea8c62458ed952d725a
Jeet_dadhania <47849684+jeet308@users.noreply.github.com>
2021-03-23 16:58:47 +0530

    Update main.py

--- marshmallow/main.py
+++ marshmallow/main.py
@@ -1,29 +1,25 @@
 from fastapi import FastAPI, UploadFile, Form, File, Request, status
 from typing import Optional
-from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 import schema as sm
 from PIL import Image
 import base64
 import time
 import os
-from io import BytesIO
 import io
 from datetime import datetime
-import json
-
 
 app = FastAPI()
 {}
+
 time_stamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
 chunk_size = (10*1024*1024)
-
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
     error_out = convert_error_string(exc.errors())
     return JSONResponse({"data": None, "status": "failed", "error": error_out}, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
-
 
 @app.post("/imagetest/",responses={200: {'model': sm.Example}})
 async def post_data(
@@ -33,17 +29,18 @@ async def post_data(
     resize_height: int = Form(...,description="image resizeheight "),
     image_format: str = Form(...,description="image file format "),
     quality_check: bool = Form(True,description="image quality check"),
-    image_file: UploadFile = File(...,description="image file"),
-):
+    image_file: UploadFile = File(...,description="image file")):
+
+
     process_start_time = time.time()
+    path = save_file(image_file)
 
     file_extension = os.path.splitext(image_file.filename)[-1]
     
-    if file_extension in ['.jpg', '.jpeg', '.png']:
+    if file_extension in ['.jpg', '.jpeg', '.png', '.tiff', '.tif', '.webp']:
 
-        image = read_imagefile(await image_file.read())
-        
-        image_file_size = len(image.fp.read())
+        image_file_size = os.path.getsize(path)
+        image = Image.open(path)
 
         try:
             input_data = sm.ImageSchema().load(
@@ -54,33 +51,42 @@ async def post_data(
                  "quality_check": quality_check,
                  "image_format": image_format
                  })
+
             if (image_file_size < chunk_size):
                 wpercent = (input_data['resize_width'] / float(image.size[0]))
                 hsize = int((float(image.size[1]) * float(wpercent)))
                 image = image.resize(
                     (input_data['resize_width'], hsize), Image.ANTIALIAS)
                 buf = io.BytesIO()
-                if file_extension in ['jpg', 'jpeg']:
+
+                if file_extension in ['.jpg', '.jpeg']:
                     image.save(buf, format='JPEG')
-                else:
+                elif file_extension in ['.png']:
                     image.save(buf, format='PNG')
+                elif file_extension in ['.tiff', '.tif']:
+                    image.save(buf, format='TIFF')
+                else:
+                    image.save(buf, format='WEBP')
+  
                 base64_string = base64.b64encode(buf.getvalue()).decode()
+                
             else:
                 return JSONResponse(
                     {"data": None,
                      "error": {
-                         "type": "input_error",
-                         "field": "image_file",
-                         "message": "file size mast be less than 10MB"
-                     },
-                        "status": "falied"
-                     }, status_code=400)
+                               "type": "input_error",
+                               "field": "image_file",
+                               "message": "file size mast be less than 10MB"
+                              },
+                     "status": "falied"
+                    }, status_code=400)
+
             data = {
                 "basestring": base64_string,
                 "reference_id": input_data['reference_id'],
                 "time_stamp": time_stamp,
-                "processtime": (time.time() - process_start_time)
-            }
+                "processtime": (time.time() - process_start_time),
+                   }
             status = "success"
             error = None
             status_code = 200
@@ -95,19 +101,24 @@ async def post_data(
         return JSONResponse(
             {"data": None,
              "error": {
-                 "type": "input_error",
-                 "field": "image_file",
-                 "message": "file formate not support"
-             },
-                "status": "falied"
-             }, status_code=400)
+                       "type": "input_error",
+                       "field": "image_file",
+                       "message": "file formate not support"
+                      },
+             "status": "falied"
+            }, status_code=400)
+            
+    os.remove(path)
     return JSONResponse({"data": data, "error": error, "status": status}, status_code=status_code)
 
 
-def read_imagefile(file):
-    image = Image.open(BytesIO(file))
-    return image
 
+
+def save_file(file):
+    file_path = f"{file.filename}"
+    with open(file_path, "wb+") as file_object:
+        file_object.write(file.file.read())
+    return file_path
 
 def convert_error_string(error):
     new_error = {}
