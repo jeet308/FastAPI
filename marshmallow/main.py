1a3f59c3c6400fbc8f38e1a1b497876b5084847a
Jeet_dadhania <47849684+jeet308@users.noreply.github.com>
2021-03-26 15:10:34 +0530

    Update main.py

--- marshmallow/main.py
+++ marshmallow/main.py
@@ -3,12 +3,14 @@ from typing import Optional
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 import schema as sch
+import support as supp
 from PIL import Image
 import base64
 import time
 import os
 import io
 from datetime import datetime
+import filetype
 
 app = FastAPI()
 {}
@@ -18,122 +20,83 @@ chunk_size = (10*1024*1024)
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    error_out = convert_error(exc.errors())
+    error_out = supp.convert_error(exc.errors())
     return JSONResponse(
         {"data": None,
          "error": error_out,
          "status": "failed"},
           status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
 
-@app.post("/testapi/",responses={200: {'model': sch.Example}})
+@app.post("/testapi/",responses={200:{'model': sch.Example_200},422: {'model': sch.Example_422},400:{'model': sch.Example_400}})
 async def post_data(
     reference_id: str = Form(...,description="Reference id"),
     company_name: str = Form(...,description="Company name"),
-    resize_width: int = Form(...,description="Image resize width "),
-    resize_height: int = Form(...,description="Image resize height "),
+    resize_width: Optional[int] = Form(None,description="Image resize width "),
+    resize_height: Optional[int] = Form(None,description="Image resize height "),
     image_format: str = Form(...,description="Image file format "),
     quality_check: bool = Form(True,description="Image quality check"),
     image_file: UploadFile = File(...,description="Image file")):
 
 
     process_start_time = time.time()
-    image_path = save_file(image_file)
+    image_path = supp.save_file(image_file)
 
-    file_extension = image_file.filename.split('.')[-1]
+    file_extension = filetype.guess(image_path)
     
-    if file_extension in ['jpg', 'jpeg', 'png', 'tiff', 'tif', 'webp']:
+    try:
+        input_data = sch.ImageSchema().load(
+            {"reference_id": reference_id,
+            "resize_width": resize_width,
+            "resize_height": resize_height,
+            "company_name": company_name,
+            "quality_check": quality_check,
+            "image_format": image_format,
+            "image_file":image_file
+            })
 
-        image_file_size = os.path.getsize(image_path)
         image = Image.open(image_path)
 
-        try:
-            input_data = sch.ImageSchema().load(
-                {"reference_id": reference_id,
-                 "resize_width": resize_width,
-                 "resize_height": resize_height,
-                 "company_name": company_name,
-                 "quality_check": quality_check,
-                 "image_format": image_format
-                 })
+        if resize_width != None:
+            width_percent = (input_data['resize_width'] / float(image.size[0]))
+            image_height = int((float(image.size[1]) * float(width_percent)))
+            image = image.resize((input_data['resize_width'], image_height), Image.ANTIALIAS)
+        else:
+            height_percent = (input_data['resize_height'] / float(image.size[1]))
+            image_width = int((float(image.size[0]) * float(height_percent)))
+            image = image.resize((input_data['resize_height'], image_width), Image.ANTIALIAS)
 
-            if (image_file_size < chunk_size):
-                width_percent = (input_data['resize_width'] / float(image.size[0]))
-                image_height = int((float(image.size[1]) * float(width_percent)))
-                image = image.resize(
-                    (input_data['resize_width'], image_height), Image.ANTIALIAS)
-                buf = io.BytesIO()
+        buf = io.BytesIO()
 
-                if file_extension in ['jpg', 'jpeg']:
-                    image.save(buf, format='JPEG')
-                elif file_extension in ['png']:
-                    image.save(buf, format='PNG')
-                elif file_extension in ['tiff', 'tif']:
-                    image.save(buf, format='TIFF')
-                else:
-                    image.save(buf, format='WEBP')
+        if file_extension in ['jpg', 'jpeg']:
+            image.save(buf, format='JPEG')
+        elif file_extension in ['png']:
+            image.save(buf, format='PNG')
+        elif file_extension in ['tiff', 'tif']:
+            image.save(buf, format='TIFF')
+        else:
+            image.save(buf, format='WEBP')
   
-                base64_string = base64.b64encode(buf.getvalue()).decode()
+        base64_string = base64.b64encode(buf.getvalue()).decode()
                 
-            else:
-                os.remove(image_path)
-                return JSONResponse(
-                    {"data": None,
-                     "error": {
-                               "type": "ValidationError",
-                               "field": "image_file",
-                               "message": "file size must be less than 10MB"
-                              },
-                     "status": "failed"
-                    }, status_code=400)
-
-            data = {
-                "base64_string": base64_string,
-                "reference_id": input_data['reference_id'],
-                "time_stamp": time_stamp,
-                "process_time": (time.time() - process_start_time),
-                   }
-            status = "success"
-            error = None
-            status_code = 200
-        except sch.ValidationError as e:
-            error_type = e.__class__.__name__
-            error_messages = convert_error_string(e.__dict__['messages'])
-            error = {"type": error_type, "fields": error_messages}
-            data = None
-            status = "falied"
-            status_code = 400
-    else:
-        os.remove(image_path)
-        return JSONResponse(
-            {"data": None,
-             "error": {
-                       "type": "ValidationError",
-                       "field": "image_file",
-                       "message": f"{file_extension} file not support"
-                      },
-             "status": "failed"
-            }, status_code=400)
+        data = {
+            "base64_string": base64_string,
+            "reference_id": input_data['reference_id'],
+            "time_stamp": time_stamp,
+            "process_time": (time.time() - process_start_time),
+               }
+        status = "success"
+        error = None
+        status_code = 200
+    except sch.ValidationError as e:
+        error_type = e.__class__.__name__
+        error_messages = supp.convert_error_string(e.__dict__['messages'])
+        error = {"type": error_type, "fields": error_messages}
+        data = None
+        status = "falied"
+        status_code = 400
+    
             
     os.remove(image_path)
     return JSONResponse({"data": data, "error": error, "status": status}, status_code=status_code)
 
 
-def save_file(file):
-    file_path = f"{file.filename}"
-    with open(file_path, "wb+") as file_object:
-        file_object.write(file.file.read())
-    return file_path
-
-def convert_error_string(error):
-    new_error = []
-    for key in error:
-        new_error.append({key: error[key]})
-    return new_error
-
-def convert_error(exc):
-    error_fields = []
-    for data in exc:
-        temp_field = data['loc'][1] if len(data['loc']) > 1 else data['loc'][0]
-        temp_mes = data['msg']
-        error_fields.append({temp_field: {'meesage': temp_mes}})
-    return {"type": "ValidationError", "fields": error_fields}
