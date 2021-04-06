c91b2f5dfd1e0b9145b079f1ead08ac260035ffa
Jeet_dadhania <47849684+jeet308@users.noreply.github.com>
2021-04-06 10:56:00 +0530

    Update schema.py

--- marshmallow/schema.py
+++ marshmallow/schema.py
@@ -12,6 +12,7 @@ chunk_size = (10*1024*1024)
 logg = GetLogger()
 log = logg.get_logger()
 
+
 class ImageSchema(Schema):
     reference_id = fields.Str()
     resize_width = fields.Int(missing=None)
@@ -24,9 +25,9 @@ class ImageSchema(Schema):
     @validates('reference_id')
     def reference_id_validate(cls, v):
         if not v.isalnum():
-            raise ValidationError("reference id must be alphanumeric")
+            raise ValidationError("reference_id must be alphanumeric")
         if len(v) != 6:
-            raise ValidationError("reference id length must be 6")
+            raise ValidationError("reference_id length must be 6")
         return v
 
     @validates('company_name')
@@ -48,13 +49,15 @@ class ImageSchema(Schema):
     def check_height_width(cls, data, **kwargs):
         if data['resize_width'] is not None and data['resize_height'] is None:
             if (data['resize_width'] < 1 or data['resize_width'] >= 1920):
-                raise ValidationError('resize width must be 1 to 1920','resize_width')
+                raise ValidationError(
+                    'resize width must be 1 to 1920', 'resize_width')
         elif data['resize_height'] is not None and data['resize_width'] is None:
             if (data['resize_height'] < 1 or data['resize_height'] >= 1920):
-                raise ValidationError('resize height must be 1 to 1920','resize_height')
+                raise ValidationError(
+                    'resize_height must be 1 to 1920', 'resize_height')
         else:
             raise ValidationError(
-                'Please use any one of resize_height or resize_width','common')
+                'Please use any one of resize_height or resize_width', 'common')
         return data
 
     @validates_schema
@@ -67,18 +70,21 @@ class ImageSchema(Schema):
         if file_extension != None:
             extension = file_extension.extension
             if extension not in ['jpg', 'jpeg', 'png', 'tiff', 'tif', 'webp']:
-                raise ValidationError('file is not supported',"image_file")
+                raise ValidationError('file is not supported', "image_file")
         else:
-            raise ValidationError('file is not supported',"image_file")
+            raise ValidationError('file is not supported', "image_file")
         if chunk_size < image_file_size:
-            raise ValidationError(f"file size must be less than 10MB","image_file")
+            raise ValidationError(
+                f"file size must be less than 10MB", "image_file")
 
         image = Image.open(image_path)
         width, height = image.size
         if width > 2000:
-            raise ValidationError(f"image width must be less than 2000 pixel","image_file")
+            raise ValidationError(
+                f"image width must be less than 2000 pixel", "image_file")
         if height > 2000:
-            raise ValidationError(f"image height must be less than 2000 pixel","image_file")
+            raise ValidationError(
+                f"image height must be less than 2000 pixel", "image_file")
 
         np_img = np.array(image)
         data["image_file"] = np_img
@@ -115,12 +121,27 @@ class Example_422(BaseModel):
                     "type": "ValidationError",
                     "fields": [
                         {
-                            "reference_id": {
-                                "message": "reference id length must be 6"
+                            "common": {
+                                "message": "Please use any one of resize_height or resize_width"
                             }
                         }
                     ]
                 },
-                "status": "falied",
+                "status": "falied"
+            }
+        }
+
+
+class Example_504(BaseModel):
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "data": "null",
+                "error": {
+                        "type": "TimeoutError",
+                        "fields": "null"
+                },
+                "status": "failed"
             }
         }
