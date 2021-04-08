1a773259f41635ccede4dbf8f4a86544b0c9400a
Jeet_dadhania <47849684+jeet308@users.noreply.github.com>
2021-04-08 09:02:57 +0530

    Update schema.py

--- marshmallow/schema.py
+++ marshmallow/schema.py
@@ -1,5 +1,4 @@
 from marshmallow import Schema, fields, validate, ValidationError, validates, validates_schema
-from pydantic import BaseModel
 import support as sup
 import os
 import filetype
@@ -7,162 +6,86 @@ from PIL import Image
 import numpy as np
 from helper import GetLogger
 
-chunk_size = (10*1024*1024)
+chunk_size = (5*1024*1024)
 
 logg = GetLogger()
 log = logg.get_logger()
-
 
 class ImageSchema(Schema):
     reference_id = fields.Str()
     resize_width = fields.Int(missing=None)
     resize_height = fields.Int(missing=None)
     company_name = fields.Str()
-    image_format = fields.Str()
+    image_format = fields.Str(validate=validate.OneOf(['jpg', 'jpeg', 'png', 'tiff', 'tif', 'webp']))
     quality_check = fields.Bool()
-    image_file = fields.Str()
+    image_file = fields.Field()
+    image_type = fields.Str()
 
-    @validates('reference_id')
-    def reference_id_validate(cls, v):
-        if not v.isalnum():
-            raise ValidationError("reference_id must be alphanumeric")
-        if len(v) != 6:
-            raise ValidationError("reference_id length must be 6")
-        return v
+    @validates_schema
+    def reference_id_validate(cls, data, **kwargs):
+        if not data['reference_id'].isalnum():
+            raise ValidationError("reference_id must be alphanumeric",'reference_id')
+        if len(data['reference_id']) != 6:
+            raise ValidationError("reference_id length must be 6",'reference_id')
+        return data
 
-    @validates('company_name')
-    def company_name_validate(cls, v):
-        if not v.isalpha():
-            raise ValidationError("company name must be alphabetic only")
-        if len(v) < 0 and len(v) < 30:
+    @validates_schema
+    def company_name_validate(cls, data, **kwargs):
+        if not data['company_name'].isalpha():
+            raise ValidationError("company name must be alphabetic only",'company_name')
+        if len(data['company_name']) < 0 and len(data['company_name']) < 30:
             raise ValidationError(
-                "company name length must be 1 to 30 characters")
-        return v
-
-    @validates('image_format')
-    def image_format_validate(cls, v):
-        if v not in ['jpg', 'jpeg', 'png', 'tiff', 'tif', 'webp']:
-            raise ValidationError(f'{v} file is not supported')
-        return v
+                "company name length must be 1 to 30 characters",'company_name')
+        return data
 
     @validates_schema
     def check_height_width(cls, data, **kwargs):
-        if data['resize_width'] is not None and data['resize_height'] is None:
-            if (data['resize_width'] < 1 or data['resize_width'] >= 1920):
-                raise ValidationError(
-                    'resize width must be 1 to 1920', 'resize_width')
-        elif data['resize_height'] is not None and data['resize_width'] is None:
-            if (data['resize_height'] < 1 or data['resize_height'] >= 1920):
-                raise ValidationError(
-                    'resize_height must be 1 to 1920', 'resize_height')
+        w = data['resize_width']
+        h = data['resize_height']
+
+        if w is not None and h is None:
+            if (w < 1 or w >= 1920):
+                raise ValidationError('resize_width must be 1 to 1920', 'resize_width')
+        elif h is not None and w is None:
+            if (h < 1 or h >= 1920):
+                raise ValidationError('resize_height must be 1 to 1920', 'resize_height')
         else:
-            raise ValidationError(
-                'Please use any one of resize_height or resize_width', 'common')
+            raise ValidationError('Please use any one of resize_height or resize_width', 'common')
         return data
 
     @validates_schema
     def image_file_validate(cls, data, **kwargs):
-        image_path = data["image_file"]
-        file_extension = filetype.guess(image_path)
-        image_file_size = os.path.getsize(image_path)
-        image_type = file_extension.mime
+        try:
+            image_file = data["image_file"]
+            image_path = sup.save_file(image_file)
+        except:
+            raise ValidationError("field required", "image_file")
 
-        if file_extension != None:
-            extension = file_extension.extension
-            if extension not in ['jpg', 'jpeg', 'png', 'tiff', 'tif', 'webp']:
-                raise ValidationError('file is not supported', "image_file")
+        file_type = filetype.guess(image_path)
+        image_file_size = os.path.getsize(image_path)
+
+        if file_type != None:
+            file_extension = file_type.extension
+            if file_extension not in ['jpg', 'jpeg', 'png', 'tiff', 'tif', 'webp']:
+                raise ValidationError('file format is not supported', "image_file")
         else:
             raise ValidationError('file is not supported', "image_file")
         if chunk_size < image_file_size:
-            raise ValidationError(
-                f"file size must be less than 10MB", "image_file")
+            raise ValidationError("file size must be less than 5 MB", "image_file")
 
-        image = Image.open(image_path)
-        width, height = image.size
+        rgba_image = Image.open(image_path)
+        rgb_image = rgba_image.convert('RGB')
+        width, height = rgb_image.size
         if width > 2000:
-            raise ValidationError(
-                f"image width must be less than 2000 pixel", "image_file")
+            raise ValidationError("image width must be less than 2000 pixel", "image_file")
         if height > 2000:
-            raise ValidationError(
-                f"image height must be less than 2000 pixel", "image_file")
+            raise ValidationError("image height must be less than 2000 pixel", "image_file")
 
-        np_img = np.array(image)
+        np_img = np.array(rgb_image)
         data["image_file"] = np_img
+        data["image_type"] = file_extension
 
-        log.debug(image_type)
+        log.debug(file_extension)
 
         return data
 
-
-class Example_200(BaseModel):
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "data": {
-                    "base64_string": "UklGRrQBAABXRUJQVlA4WAoAAAAQAAAAMQAAGwAAQUxQSCIAAAABF7D9EREBo7aRJM21i3AMcoN2nxH9jwuA+VVVVe9PvACYVlA4IGwBAAAQBwCdASoyABwAPm02l0gkIyIhJApIgA2JZz5owtVXjtBBBHiINLjBN5jWSk49bfuJCqbYWZ7XW1WgnN6Clg6KAAD+2+qf9rnRIkhj5SmyCEU/2vDv4WLuPCtPZ0z85RPS9UT/DdgDiJb6ct90KwpiwVQimg8xQS/VYxIvE9M8po5eIS3LUblYCy8DzHHoQfeplwBKR5JWqHnspQID7fH2iHg8S3aXsGev4FcljKc9mrRQfMAszhNlGzyPnHoMAxDvX8ch1K+4YcuHbl+ST6UyvgPQBvc+Xrptb7SSE3y4zr7bfk38PYavkTlPun5O4+HfJ5KPZ+Eo9Hevw7jNfWN7F7C9C6FRCPRynuyu76+TGhZBN/7Te4OYrgYAaGqqr4s0jDFxgY9RRSZKF47gokXqE8dQsPaAAMgMfW0FPf/pIp9gv1k9fhLEXk3p4lpLZnsESgWs703hpIm9x99VsTEkMFwZOunZvlN+QAAA",
-                    "reference_id": "abcd12",
-                    "time_stamp": "2021-03-31T14:26:13",
-                    "process_time": 0.08904647827148438
-                },
-                "error": "null",
-                "status": "success",
-            }
-        }
-
-
-class Example_400(BaseModel):
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "data": "null",
-                "error": {
-                    "type": "ValidationError",
-                    "fields": [
-                        {
-                            "common": {
-                                "message": "Please use any one of resize_height or resize_width"
-                            }
-                        }
-                    ]
-                },
-                "status": "falied"
-            }
-        }
-
-
-class Example_422(BaseModel):
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "data": "null",
-                "error": {
-                    "type": "ValidationError",
-                    "fields": [
-                        {
-                            "quality_check": {
-                                "meesage": "value could not be parsed to a boolean"
-                            }
-                        }
-                    ]
-                },
-                "status": "failed"
-            }
-        }
-
-
-class Example_504(BaseModel):
-
-    class Config:
-        schema_extra = {
-            "example": {
-                "data": "null",
-                "error": {
-                        "type": "TimeoutError",
-                        "fields": "null"
-                },
-                "status": "failed"
-            }
-        }
