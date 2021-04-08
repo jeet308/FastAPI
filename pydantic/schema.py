1b1fddaef5376b01fe3bef54a5cc701bc2318e81
Jeet_dadhania <47849684+jeet308@users.noreply.github.com>
2021-04-08 09:06:24 +0530

    Create schema.py

---
+++ pydantic/schema.py
@@ -0,0 +1,88 @@
+from pydantic import BaseModel, ValidationError, validator, root_validator
+from fastapi import UploadFile
+from typing import Optional
+import support as sup
+import os
+import filetype
+from PIL import Image
+import numpy as np
+from helper import GetLogger
+
+chunk_size = (5*1024*1024)
+
+logg = GetLogger()
+log = logg.get_logger()
+
+class ImageSchema(BaseModel):
+    reference_id : str
+    resize_width : Optional[int] = None
+    resize_height : Optional[int] = None
+    company_name : str
+    image_format : str
+    quality_check : bool
+    image_file : UploadFile = None
+
+    @validator('reference_id')
+    def reference_id_validate(cls, v):
+        if not v.isalnum():
+            raise ValueError("reference_id must be alphanumeric")
+        if len(v) != 6:
+            raise ValueError("reference_id length must be 6")
+        return v
+
+    @validator('company_name')
+    def company_name_validate(cls, v):
+        if not v.isalpha():
+            raise ValueError("company_name must be alphabetic only")
+        if len(v) < 0 and len(v) < 30:
+            raise ValueError("company_name length must between 1 to 30 characters")
+        return v
+
+    @root_validator()
+    def check_height_width(cls, values):
+        w = values.get('resize_width')
+        h = values.get('resize_height')
+
+        if w is not None and h is None:
+            if (w < 1 or w >= 1920):
+                raise ValueError('resize_width must be 1 to 1920')
+        elif h is not None and w is None:
+            if (h < 1 or h >= 1920):
+                raise ValueError('resize_height must be 1 to 1920')
+        else:
+            raise ValueError('Please use any one of resize_height or resize_width')
+
+        return values
+
+    @validator('image_file')
+    def image_file_validate(cls, v):
+        try:
+            image_path = sup.save_file(v)
+        except:
+            raise ValueError("field required")
+
+        file_type = filetype.guess(image_path)
+        image_file_size = os.path.getsize(image_path)
+
+        if file_type != None:
+            file_extension = file_type.extension
+            if file_extension not in ['jpg', 'jpeg', 'png', 'tiff', 'tif', 'webp']:
+                raise ValidationError('file format is not supported', "image_file")
+        else:
+            raise ValueError('file is not supported')
+        if chunk_size < image_file_size:
+            raise ValueError("file size must be less than 5 MB")
+
+        image = Image.open(image_path)
+        width, height = image.size
+        if width > 2000:
+            raise ValueError("image width must be less than 2000 pixel")
+        if height > 2000:
+            raise ValueError("image height must be less than 2000 pixel")
+
+        np_img = np.array(image)
+
+        log.debug(file_extension)
+
+        return np_img
+
