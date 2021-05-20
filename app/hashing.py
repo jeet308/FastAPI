d8900584bf0b7de1fef8f76b97673e69708ec691
jeet <jeetdadhania308@gmail.com>
2021-05-20 10:40:36 +0530

    hashing

--- app/hashing.py
+++ app/hashing.py
@@ -8,5 +8,5 @@ def bcrypt(password: str):
     return pwd_cxt.hash(password)
 
 
-def verify_password(hashed_password, plain_password):
+def verify_password(plain_password, hashed_password):
     return pwd_cxt.verify(plain_password, hashed_password)
