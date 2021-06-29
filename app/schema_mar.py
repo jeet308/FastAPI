5ec8e1a554f73f30da69f948644b8bb7da2c8018
jeet <jeetdadhania308@gmail.com>
2021-06-29 10:12:23 +0530

    pre-commit setup

--- app/schema_mar.py
+++ app/schema_mar.py
@@ -1,53 +1,64 @@
-from marshmallow import Schema, fields, validate, ValidationError, validates, validates_schema
-import utils as sup
 import os
-import filetype
-from PIL import Image
-import numpy as np
-from loguru import logger
 
-chunk_size = (5*1024*1024)
+import filetype
+import numpy as np
+import utils as sup
+from loguru import logger
+from marshmallow import Schema, ValidationError, fields, validate, validates_schema
+from PIL import Image
+
+chunk_size = 5 * 1024 * 1024
+
 
 class ImageSchema(Schema):
     reference_id = fields.Str()
     resize_width = fields.Int(missing=None)
     resize_height = fields.Int(missing=None)
     company_name = fields.Str()
-    image_format = fields.Str(validate=validate.OneOf(['jpg', 'jpeg', 'png', 'tiff', 'tif', 'webp']))
+    image_format = fields.Str(
+        validate=validate.OneOf(["jpg", "jpeg", "png", "tiff", "tif", "webp"])
+    )
     quality_check = fields.Bool()
     image_file = fields.Field()
     image_type = fields.Str()
 
     @validates_schema
     def reference_id_validate(cls, data, **kwargs):
-        if not data['reference_id'].isalnum():
-            raise ValidationError("reference_id must be alphanumeric",'reference_id')
-        if len(data['reference_id']) != 6:
-            raise ValidationError("reference_id length must be 6",'reference_id')
+        if not data["reference_id"].isalnum():
+            raise ValidationError("reference_id must be alphanumeric", "reference_id")
+        if len(data["reference_id"]) != 6:
+            raise ValidationError("reference_id length must be 6", "reference_id")
         return data
 
     @validates_schema
     def company_name_validate(cls, data, **kwargs):
-        if not data['company_name'].isalpha():
-            raise ValidationError("company name must be alphabetic only",'company_name')
-        if len(data['company_name']) < 0 and len(data['company_name']) < 30:
+        if not data["company_name"].isalpha():
             raise ValidationError(
-                "company name length must be 1 to 30 characters",'company_name')
+                "company name must be alphabetic only", "company_name"
+            )
+        if len(data["company_name"]) < 0 and len(data["company_name"]) < 30:
+            raise ValidationError(
+                "company name length must be 1 to 30 characters", "company_name"
+            )
         return data
 
     @validates_schema
     def check_height_width(cls, data, **kwargs):
-        w = data['resize_width']
-        h = data['resize_height']
+        w = data["resize_width"]
+        h = data["resize_height"]
 
         if w is not None and h is None:
-            if (w < 1 or w >= 1920):
-                raise ValidationError('resize_width must be 1 to 1920', 'resize_width')
+            if w < 1 or w >= 1920:
+                raise ValidationError("resize_width must be 1 to 1920", "resize_width")
         elif h is not None and w is None:
-            if (h < 1 or h >= 1920):
-                raise ValidationError('resize_height must be 1 to 1920', 'resize_height')
+            if h < 1 or h >= 1920:
+                raise ValidationError(
+                    "resize_height must be 1 to 1920", "resize_height"
+                )
         else:
-            raise ValidationError('Please use any one of resize_height or resize_width', 'common')
+            raise ValidationError(
+                "Please use any one of resize_height or resize_width", "common"
+            )
         return data
 
     @validates_schema
@@ -61,22 +72,33 @@ class ImageSchema(Schema):
         file_type = filetype.guess(image_path)
         image_file_size = os.path.getsize(image_path)
 
-        if file_type != None:
+        if file_type is not None:
             file_extension = file_type.extension
-            if file_extension not in ['jpg', 'jpeg', 'png', 'tiff', 'tif', 'webp']:
-                raise ValidationError('file format is not supported', "image_file")
+            if file_extension not in [
+                "jpg",
+                "jpeg",
+                "png",
+                "tiff",
+                "tif",
+                "webp",
+            ]:
+                raise ValidationError("file format is not supported", "image_file")
         else:
-            raise ValidationError('file is not supported', "image_file")
+            raise ValidationError("file is not supported", "image_file")
         if chunk_size < image_file_size:
             raise ValidationError("file size must be less than 5 MB", "image_file")
 
         rgba_image = Image.open(image_path)
-        rgb_image = rgba_image.convert('RGB')
+        rgb_image = rgba_image.convert("RGB")
         width, height = rgb_image.size
         if width > 2000:
-            raise ValidationError("image width must be less than 2000 pixel", "image_file")
+            raise ValidationError(
+                "image width must be less than 2000 pixel", "image_file"
+            )
         if height > 2000:
-            raise ValidationError("image height must be less than 2000 pixel", "image_file")
+            raise ValidationError(
+                "image height must be less than 2000 pixel", "image_file"
+            )
 
         np_img = np.array(rgb_image)
         data["image_file"] = np_img
--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from fastapi.responses import JSONResponse
-from fastapi import FastAPI, status, Request
-from fastapi.exceptions import RequestValidationError
-from app import utils, models
-from app.database import engine
-from app.router import authentication, client, image_base64
-import log
 import asyncio
 import uuid
 
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
+import log
+from app import models, utils
+from app.database import engine
+from app.router import authentication, client, image_base64
 
 app = FastAPI()
-
-
 logger = log._init_logger()
-
 models.Base.metadata.create_all(engine)
 
 
@@ -22,32 +20,47 @@ async def request_middleware(request, call_next):
     end_point = request.url.path
     request_id = str(uuid.uuid4())
     with logger.contextualize(request_id=request_id, end_point=end_point):
-        logger.debug('--------------start--------------')
+        logger.debug("--------------start--------------")
 
         try:
             return await call_next(request)
         except Exception as ex:
             print(ex)
             logger.error(f"Request failed: {ex}")
-            return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                                content={"error": {"type": "UnknownError",
-                                                   "message": "Unknown error found.",
-                                                   "fields": None}, "status": "failed"})
+            return JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={
+                    "error": {
+                        "type": "UnknownError",
+                        "message": "Unknown error found.",
+                        "fields": None,
+                    },
+                    "status": "failed",
+                },
+            )
         finally:
-            logger.debug('---------------end---------------')
+            logger.debug("---------------end---------------")
 
 
 @app.exception_handler(utils.ValidationException)
-async def validation_exception_handler(request: Request, exc: utils.ValidationException):
-    return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                        content={"error": exc.errors_out, "status": "failed"})
+async def validation_exception_handler(
+    request: Request, exc: utils.ValidationException
+):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"error": exc.errors_out, "status": "failed"},
+    )
 
 
 @app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
+async def validation_exception_handler_error(
+    request: Request, exc: RequestValidationError
+):
     error_out = utils.convert_error_pydantic(exc.errors())
-    return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-                        content={"error": error_out, "status": "failed"})
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"error": error_out, "status": "failed"},
+    )
 
 
 @app.middleware("http")
@@ -55,13 +68,19 @@ async def timeout_middleware(request: Request, call_next):
     try:
         return await asyncio.wait_for(call_next(request), timeout=1000)
     except asyncio.TimeoutError:
-        return JSONResponse(status_code=status.HTTP_504_GATEWAY_TIMEOUT,
-                            content={"error": {"type": "TimeoutError", "message": "API timed out.", "fields": None},
-                                     "status": "failed"})
+        return JSONResponse(
+            status_code=status.HTTP_504_GATEWAY_TIMEOUT,
+            content={
+                "error": {
+                    "type": "TimeoutError",
+                    "message": "API timed out.",
+                    "fields": None,
+                },
+                "status": "failed",
+            },
+        )
 
 
 app.include_router(authentication.router)
 app.include_router(client.router)
 app.include_router(image_base64.router)
-
-
